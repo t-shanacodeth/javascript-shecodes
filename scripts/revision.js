3881b75adbb3a335d87ee2c6d31cb307f3d50a29
Tishana B. <t.bascombe@outlook.com>
2025-06-03 13:30:54 +0000

    add js trim

--- scripts/revision.js
+++ scripts/revision.js
@@ -1,7 +1,8 @@
 let firstName = prompt("What is your first name?");
+firstName = firstName.trim();
 
 if (firstName.length > 0) {
-  alert("Welcome, " + firstName.trimStart() + "!");
+  alert("Welcome, " + firstName + "!");
 } else {
   alert("Too bad for you!");
 }
